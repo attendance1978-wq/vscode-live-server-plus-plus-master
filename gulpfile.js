c0b558fd85007b06eba25d25add5e97dfac7486b
attendance1978-wq <attendance1978@gmail.com>
2025-11-27 01:12:18 -0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,10 @@ const { src, dest, series } = require('gulp');
 const ts = require('gulp-typescript');
 const tsProject = ts.createProject('tsconfig.json');
 
-// Compile TypeScript
 function buildTS() {
     return tsProject.src()
         .pipe(tsProject())
         .js.pipe(dest('out'));
 }
 
-// Default task
 exports.default = series(buildTS);
